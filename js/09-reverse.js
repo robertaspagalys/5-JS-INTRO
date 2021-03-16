72ef9a3ea1a2a790ac1fb24b9afc699405a865b1
Robertas <robertux19@gmail.com>
2021-03-16 15:27:01 +0200

    reverseUp

--- js/09-reverse.js
+++ js/09-reverse.js
@@ -28,3 +28,17 @@ console.log(text, '->', reverse);
 //}
 
 //console.log(text, '->', reverse);
+
+
+
+function reverseString(text) {
+let reverse = '';
+for (let i = 0; i < text.length; i++) {
+reverse = text[i] + reverse;
+}
+console.log(text, '->', reverse);
+}
+const pirmasZodis = 'Labutis';
+reverseString(pirmasZodis)
+
+reverseString('Klaipeda')
