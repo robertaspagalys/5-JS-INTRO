03557754f8e1c15d2f23b086025fcd5ab2ba67d5
Robertas <robertux19@gmail.com>
2021-03-16 11:13:55 +0200

    liekana

---
+++ js/09-reverse.js
@@ -0,0 +1,30 @@
+/*panaudojant ciklą perrašyti tekstinio tipo kintamųjų reikšmes iš kito galo:
+pvz.: “abcdef” -> “fedcba”
+*/
+
+const text = 'abcdef';
+let reverse = '';
+
+for (let i = 0; i < text.length; i++) {
+    reverse = text[i] + reverse;
+}
+console.log(text, '->', reverse);
+
+//for (let i = 0; i < text.length; i++) {
+    //console.log(i, text[i]);
+    //reverse += text[i];
+//}
+
+//console.log(text, '->', reverse);
+
+//for (let i = text.length -1; i >= 0; i--) {
+//    reverse += text[i];
+//}
+
+//console.log(text, '->', reverse);
+
+//for (let i = text.length; i > 0; i--) {
+//    reverse += text[i - 1];
+//}
+
+//console.log(text, '->', reverse);
